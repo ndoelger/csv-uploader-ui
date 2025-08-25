5729b2869e7768ffdb15bceb01539f97503ca7bd
Nic Doelger <doelgern@gmail.com>
2025-08-25 15:04:56 -0400

    tsconfig + env

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+const domain: string = import.meta.env.VITE_DOMAIN || '';
+
+console.log(domain)
+const clientId: string = import.meta.env.VITE_CLIENT_ID || '';
+console.log(clientId)
+
 createRoot(document.getElementById('root')!).render(
   <Auth0Provider
-    domain="dev-tm4xrzwhmq41km04.us.auth0.com"
-    clientId="RqBl7W7Ybddt8KMClLVPcfbsrP95osFM"
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{ redirect_uri: window.location.origin }}
   >
     <App />
